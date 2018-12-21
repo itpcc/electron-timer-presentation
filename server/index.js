b9bf85208db61fef4fb70c4d3888fa9c7b7ba9d1
chunza2542 <chunza2542@hotmail.com>
2018-12-21 15:53:21 +0700

    Refactored server code

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+const PORT = process.env.PORT || 3000;
 const PASSWORD = process.env.COUNTDOWN_PASSWORD || "secret";
 
-server.listen(port, () => {
-	console.log('Server listening at port %d, authen password %s', port, PASSWORD);
+server.listen(PORT, () => {
+	console.log('Server listening at port %d, authen password %s', PORT, PASSWORD);
 });
 
-// Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
 let currentState = {
@@ -24,18 +22,17 @@ let currentMessage = '';
 let currentFinishTime = null;
 
 io.on('connection', (socket) => {
-	console.log('new connection', socket.id);
+	console.log('New connection', socket.id);
 
 	function calculateCurrentState(){
 		let timeDiffMillisec = currentFinishTime - (new Date());
-		console.log("currentFinishTime", currentFinishTime);
-		console.log(timeDiffMillisec)
-		if(timeDiffMillisec > 0){
+
+		if (timeDiffMillisec > 0) {
 			return Object.assign(currentState, {
 				hour:    parseInt((timeDiffMillisec) / ( 1000 * 60    * 60)),
 				minute:  parseInt((timeDiffMillisec) / ( 1000 * 60 )) % 60,
 				second:  parseInt(timeDiffMillisec  /   1000) % 60,
-				millisec: timeDiffMillisec  %   1000
+				millisec: timeDiffMillisec  %   1000,
 			});
 		}else{
 			return { code: '03',codeDesc:'timeReset' };
@@ -47,68 +44,64 @@ io.on('connection', (socket) => {
 			return socket.emit('onError', { message: 'Authentication Error' });
 		}
 
-		console.log('timer-event <', data);	
-
-		if(data.code === '01'){ // Start
-			console.log("currentState", currentState.code);
+		if (data.code === '01') {
 			if(currentState.code !== '00' && currentState.code !== '02')
 				return socket.emit('onError', { message: 'Time must set before start' });
 
-			currentFinishTime = new Date();
+			currentFinishTime = new Date()
 			currentFinishTime.setHours       (currentFinishTime.getHours()        + (parseInt(currentState.hour    ) || 0));
 			currentFinishTime.setMinutes     (currentFinishTime.getMinutes()      + (parseInt(currentState.minute  ) || 0));
 			currentFinishTime.setSeconds     (currentFinishTime.getSeconds()      + (parseInt(currentState.second  ) || 0));
 			currentFinishTime.setMilliseconds(currentFinishTime.getMilliseconds() + (parseInt(currentState.millisec) || 0));
-			console.log("currentFinishTime", currentFinishTime);
 
 			currentState = Object.assign(currentState, data);
-		}else if(data.code === '02'){ // pause
+		} else if(data.code === '02') {
 			if(currentState.code !== '01' || currentFinishTime === null)
-				return socket.emit('error', { message: 'Time must start before pause' });
+				return socket.emit('onError', { message: 'Time must start before pause' });
 
 			currentState = Object.assign(calculateCurrentState(), data);
 			currentFinishTime = null;
 		}else{
 			currentState = data;
-			if(data.code === '00'){
+			if (data.code === '00') {
 				currentState.timeSet = {
 					"hour":     parseInt(currentState.hour     ) || 0,
 					"minute":   parseInt(currentState.minute   ) || 0,
 					"second":   parseInt(currentState.second   ) || 0,
-					"millisec": parseInt(currentState.millisec ) || 0
+					"millisec": parseInt(currentState.millisec ) || 0,
 				}
 			}
 		}
-		console.log('timer-event >', currentState);
+
 		socket.broadcast.emit('timer-event', currentState);
 		socket.emit('timer-event', currentState);
 	});
+
 	socket.on('message', (data) => {
 		if (data.authenPassword === undefined || data.authenPassword !== PASSWORD) {
 			return socket.emit('onError', { message: 'Authentication Error' });
 		}
 
 		const msg = data.message.trim();
-		if(msg !== currentMessage){
+
+		if (msg !== currentMessage) {
 			currentMessage = msg;
 			socket.broadcast.emit('message', currentMessage);
 			socket.emit('message', currentMessage);
 		}
-	})
+	});
+
 	socket.on('currentStatus', () => {
 		let resp;
-		if(currentState.code === '01'){
+		if (currentState.code === '01') {
 			resp = calculateCurrentState();
-		}else{
+		} else {
 			resp = currentState;
 		}
 		resp = Object.assign(resp, {
 			'message': currentMessage
 		})
-		console.log('currentStatus >', resp);
+
 		socket.emit('currentStatus', resp);
-	})
-	// when the user disconnects.. perform this
-	socket.on('disconnect', () => {
 	});
 });
